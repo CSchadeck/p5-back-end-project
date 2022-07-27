82f14ccc9abc2530e994115ca384da850c9f69f9
Corey Schadeck <corey@schadeck.com>
2022-07-27 17:41:45 -0400

    backend fixes3

--- app/serializers/request_serializer.rb
+++ app/serializers/request_serializer.rb
@@ -1,5 +1,5 @@
 class RequestSerializer < ActiveModel::Serializer
-  attributes :id, :title, :user_id, :description, :start_date, :end_date, :accepted, :accepted_by
+  attributes :id, :title, :user_id, :image, :description, :start_date, :end_date, :accepted, :accepted_by
   has_many :connections
   belongs_to :user
 end
