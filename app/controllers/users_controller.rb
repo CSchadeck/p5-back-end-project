443be7becdc0318430877b26c095a30bc27c8aa3
Corey Schadeck <corey@schadeck.com>
2022-07-28 22:48:11 -0400

    back end fixes8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    skip_before_action :authorize, only: [:create, :index, :showWithOutLogIn]
+    #skip_before_action :authorize, only: [:create, :index,,  :showWithOutLogIn]
 
     def index
         render json: User.all
