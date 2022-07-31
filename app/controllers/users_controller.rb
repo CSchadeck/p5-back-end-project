aab2794c29241b9adbec60f2f7fc71907a031855
Corey Schadeck <corey@schadeck.com>
2022-07-31 18:50:13 -0400

    CORS bug issues6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,15 +21,15 @@ class UsersController < ApplicationController
     end
 
     def create
-        user = User.create!(user_params)
-        session[:user_id] = user.id
-        render json: user, status: :created
         #user = User.create!(user_params)
-        #if user.valid?
-        #  session[:user_id] = user.id
-         # render json: user, status: :created
-        #else
-         # render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+        #session[:user_id] = user.id
+        #render json: user, status: :created
+        user = User.create!(user_params)
+        if user.valid?
+          session[:user_id] = user.id
+          render json: user, status: :created
+        else
+          render json: { error: user.errors.full_messages }, status: :unprocessable_entity
        end
     end
 
