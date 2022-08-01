3d956a201ce5e800324ec93a33b54e2fadd820e5
Corey Schadeck <corey@schadeck.com>
2022-07-31 23:47:18 -0400

    users can now be updated by user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,49 @@
 class UsersController < ApplicationController
    # skip_before_action :authorize, only: [:create, :index, :update , :show, :showWithOutLogIn]
 
-    def index
-        render json: User.all
-    end
-
-    def showWithOutLogIn
-        render json: User.find(params[:id])
-    end
-
-    def show     
-        user = User.find_by(id: session[:user_id])
-        render json: user
-    end
-   
-    def create
-        user = User.create!(user_params)
-        if user.valid?
-          session[:user_id] = user.id
-          render json: user, status: :created
-        else
-          render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+        def index
+            render json: User.all
         end
-    end
 
-    def current_user
-        @user= User.find_by(id: session[:user_id])
-        if @user
-            render json: @user,  status: 200
+        def showWithOutLogIn
+            render json: User.find(params[:id])
         end
-    end
+        
+        def show
+            user = User.find_by_id(params[:id])
+            if user
+                render json: user.to_json 
+            else
+                render json: {error: "User not found"}, status: :not_found
+            end
+        end
+    
+        def create
+            user = User.create!(user_params)
+            if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+            else
+            render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+            end
+        end
 
-    private
-    def user_params
-      params.permit(:username, :password, :password_confirmation, :email, :bio)
-    end
+        def current_user
+            @user= User.find_by(id: session[:user_id])
+            if @user
+                render json: @user,  status: 200
+            end
+        end
+
+        def update
+            user = User.find(params[:id])
+            user.update(user_params)
+            render json: user
+        end
+
+        private
+        def user_params
+        params.permit(:username, :password, :password_confirmation, :email, :bio)
+        end
 
 end
